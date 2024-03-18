dd104436ed77873cfa78bab6c3066fdbce39e21b
janousek2 <jirik.janousek1@gmail.com>
2024-03-18 08:22:39 +0100

    FINAL

--- src/Piskvorky.java
+++ src/Piskvorky.java
@@ -274,5 +274,10 @@ public class Piskvorky {
      @param row A row of moves.
      @param col A col of moves.
      */
+    public static void makeMove(int row, int col) {
+        board[row][col] = currentPlayer;
+        lastmoverow = row;
+        lastmovecol = col;
 
+    }
 }
